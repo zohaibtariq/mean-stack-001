2882477e41ad0b7e54703c7ce2b278aa3ee86256
Zohaib Tariq <se.zohaib@gmail.com>
2017-12-03 04:47:20 +0500

    server and database setup done

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 /**
  * Created by zohaib on 12/3/2017.
  */
+
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+const config = require('./config/database');
+
+mongoose.Promise = global.Promise;
+
+//mongoose.connect(config.uri, (err) => {
+mongoose.createConnection(config.uri, (err) => {
+    useMongoClient: true;
+    if(err)
+        console.log('Failed to connect with database : ', err);
+    else
+        //console.log(config.secret);
+        console.log('Connected to database : ' + config.db);
+});
 
 app.get('*', (req, res) => {
     res.send('hello world...');
